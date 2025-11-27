85e794c68c90561628dd14a1976e6eb118a4375f
Saberkhawla-coder <khawla.saber06@gmail.com>
2025-11-27 17:06:42 +0100

    ..

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import EventsSearch from "./components/Events/EventsSearch";
+import EventDetails from "./components/Events/EventDetails";
 
 function App() {
- 
   return (
     <div>
-      <h1 class="text-3xl font-bold underline" style={{color:"white"}}>Hi</h1>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/event" element={<EventsSearch />} />
+         <Route path="/EventDetails" element={<EventDetails />} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
