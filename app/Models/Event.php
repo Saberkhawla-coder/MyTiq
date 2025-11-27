bf083545fb9e0b05e4f5058be7c5e0d388f21dc2
houda-LQ <houda.loqmane02@gmail.com>
2025-11-27 14:45:19 +0100

    Migrations + models + Authentification +event & listener

---
+++ app/Models/Event.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'date',
+        'capacity',
+        'price',
+    ];
+
+    public function tickets()
+    {
+        return $this->hasMany(Ticket::class);
+    }
+}
+
